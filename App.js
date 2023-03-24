2772a59fe85a45d04e3faaf16cd6f8dc54b3188d
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2023-03-24 19:31:04 -0300

    add expo webpack

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { SafeAreaView, StatusBar } from 'react-native';
 
-import styles from './style';
+import styles from './style'; // style App.js
 import Main from './src/components/Main';
 import HeaderTitle from './src/components/HeaderTitle';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+
       <StatusBar
         backgroundColor='white'
         barStyle="dark-content" // black icons
       />
-      
+
       <HeaderTitle />
       <Main />
+
     </SafeAreaView>
   );
 }
